042ae1366407003a060c08232e258d693dee2550
gabriel arguello <alexander.arguello@hotmail.com>
2022-08-30 15:18:01 -0400

    conexion a DB establecida y funcional

--- api/index.js
+++ api/index.js
@@ -1,15 +1,14 @@
+require('./mongo')
 const express = require('express')
-// const middlewares = require('./middleware/middlewares')
+
 const api = require('./routes')
 const app = express()
-
 app.use('/api', api)
 app.use('/', (req, res) => {
   res.json({
     mensaje: 'hola mundo'
   })
 })
-
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`)
