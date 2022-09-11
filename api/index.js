b7451371b15dde07f6308d4e4602ac47f2497ff8
gabriel arguello <alexander.arguello@hotmail.com>
2022-09-11 14:25:26 -0400

    minor errhandlr

--- api/index.js
+++ api/index.js
@@ -1,33 +1,14 @@
 require('./mongo')
 const express = require('express')
 const middleware = require('./middleware/middlewares.js')
-// const handleErrors = require('./middleware/handleErrors.js')
-
 const api = require('./routes')
 const app = express()
-
-
 app.use(express.json())
 app.use('/api', api)
-
-
-//*ruta default
-// app.use('/', (req, res) => {
-//   res.json({
-//     mensaje: 'API en construccion'
-//   })
-// })
-
-
 //*ruta 404 not found */
-//todo: realizar esta funcion como middleware e importar a index
-
 app.use(middleware.notFound)
+//*manejo de errores */
 app.use(middleware.errorHandler)
-
-// app.use(notFound)
-
-// app.use(handleErrors)
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`)
